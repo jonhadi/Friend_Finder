3763820547d920aa341cb25eb55b77b57758cdd6
jonhadi <hadijonathan@gmail.com>
2019-08-10 18:40:42 -0700

    fixed images and sizing

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,8 +6,8 @@ var profiles = [
         scores:[5,1,4,4,5,1,2,5,4,1]
     },
     {
-        name: "Jon",
-        imgURL: "https://www.everydayknow.com/wp-content/uploads/2017/07/shutterstock_549539365.jpg",
+        name: "Thanos",
+        imgURL: "https://www.sideshow.com/storage/product-images/903429/thanos_marvel_feature.jpg",
         scores: [3,3,3,3,3,3,3,3,3,3]
     },
     {
@@ -17,8 +17,13 @@ var profiles = [
     },
     {
         name: "Noob",
-        imgURL: "https://upload.wikimedia.org/wikipedia/en/thumb/2/29/Count_von_Count_kneeling.png/200px-Count_von_Count_kneeling.png",
+        imgURL: "https://giantbomb1.cbsistatic.com/uploads/scale_small/8/83576/2299992-halo4_mc_ava.png",
         scores: [1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name: "Jon",
+        imgURL: "https://www.everydayknow.com/wp-content/uploads/2017/07/shutterstock_549539365.jpg",
+        scores: [2, 3, 2, 5, 3, 1, 4, 5, 4, 4]
     }
 ];
 
